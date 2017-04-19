1428189b41e78c509a6ea667560fa5ae5deed3ea
Davron Aliyev <davronaliyev@gmail.com>
2017-04-19 13:00:06 -0400

    driver update for IE browser

--- WAF/Framework/BaseClasses/Driver.cs
+++ WAF/Framework/BaseClasses/Driver.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.Threading;
@@ -10,32 +11,46 @@ namespace WAF.Framework.BaseClasses
     {
         private static IWebElement element;
         private static SelectElement select;
-        public static void InsertText(By locator, string text)
+        internal static void InsertText(By locator, string text)
         {
             element = GetVisibleElement(locator);
             element.Clear();
             element.SendKeys(text);
             ReportHelper.PassLog("Successfully <b>' " + text + "' </b> text inserted into: <b>" + locator.ToString());
         }
-        public static void ClickOn(By locator)
+        // Use this block of code is for IE browser on Click event.
+        internal static void ClickOn(By locator)
         {
             element = GetClickableElement(locator);
-            element.Click();
-            ReportHelper.PassLog("Successfully clicked on: <b>" + locator.ToString());
+            Actions actions = new Actions(Instance);
+            actions.MoveToElement(element).Click().Perform();
+            ReportHelper.PassLog("Successfully Clicked on: <b>" + locator.ToString());
         }
-        public static void PressEnter(By locator)
+        // Use this block of code can be use for other browsers other then IE for Click event.
+        //public static void ClickOn(By locator)
+        //{
+        //    element = GetClickableElement(locator);
+        //    element.Click();
+        //    // This block of code need it if first click is not working and need second click on IE browser.
+        //    //if (VerifyElement.IsElementPresent(locator))
+        //    //{
+        //    //    element.Click();
+        //    //}
+        //    ReportHelper.PassLog("Successfully clicked on: <b>" + locator.ToString());
+        //}
+        internal static void PressEnter(By locator)
         {
             element = GetClickableElement(locator);
             element.SendKeys(Keys.Enter);
-            ReportHelper.PassLog("Successfully pressed on: <b>" + locator);
+            ReportHelper.PassLog("Successfully Pressed Enter on: <b>" + locator);
         }
-        public static void SelectFromDropdown(By locator, string value)
+        internal static void SelectFromDropdown(By locator, string value)
         {
             select = new SelectElement(GetClickableElement(locator));
             select.SelectByText(value);
             ReportHelper.PassLog("Successfully <b>' " + value + "' </b> dropdown option selected from: <b>" + locator.ToString());
         }
-        public static void WaitForElement(By locator)
+        internal static void WaitForElement(By locator)
         {
             try
             {
@@ -43,14 +58,14 @@ namespace WAF.Framework.BaseClasses
             }
             catch (Exception)
             {
-                throw new NoSuchElementException("Element is not found: <br>" + locator.ToString());
+                throw new NoSuchElementException("Element is not found: <b>" + locator.ToString());
             }
         }
-        public static void Wait(int seconds)
+        internal static void Wait(int seconds)
         {
             Thread.Sleep(seconds / 100);
         }
-        public static IWebElement GetVisibleElement(By locator)
+        internal static IWebElement GetVisibleElement(By locator)
         {
             try
             {
@@ -62,10 +77,10 @@ namespace WAF.Framework.BaseClasses
             }
             catch (Exception)
             {
-                throw new NoSuchElementException("Element is not found: <br>" + locator.ToString());
+                throw new NoSuchElementException("Element is not found: <b>" + locator.ToString());
             }
         }
-        public static IWebElement GetClickableElement(By locator)
+        internal static IWebElement GetClickableElement(By locator)
         {
             try
             {
@@ -77,8 +92,37 @@ namespace WAF.Framework.BaseClasses
             }
             catch (Exception)
             {
-                throw new NoSuchElementException("<br>Can not click the element: " + locator.ToString());
+                throw new NoSuchElementException("Can not click the element: <b>" + locator.ToString());
             }
         }
+        internal static void SelectCheckbox(By locator)
+        {
+
+
+            element = GetClickableElement(By.XPath("html/body/div[6]/div/div/div[3]/div[1]/div[2]/button[1]"));
+            Actions actions = new Actions(Instance);
+            actions.MoveToElement(element).SendKeys(Keys.Enter).Perform();
+
+
+
+            element = Instance.FindElement(locator);
+
+
+            if (!element.Selected)
+            {
+                element.Click();
+                //((IJavaScriptExecutor)Instance).ExecuteScript("arguments[0].click();", element);
+            }
+
+            GetClickableElement(locator);
+            Instance.FindElement(locator).Click();
+
+            //ReportHelper.PassLog("Successfully clicked on checkbox: <b>" + locator.ToString());
+
+            element = GetClickableElement(By.XPath("(//input[@type='checkbox'])[8]"));
+            element.Click();
+
+        }
+
     }
 }
